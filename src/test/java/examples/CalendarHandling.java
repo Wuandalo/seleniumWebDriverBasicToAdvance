bb41058b395360278d05df48425ea447e88340d9
Gustavo Henrique <gustavo_henriquejr@live.com>
2023-11-12 20:53:21 +0000

    Adding more funcionalities to handling with calendar

--- src/test/java/examples/CalendarHandling.java
+++ src/test/java/examples/CalendarHandling.java
@@ -1,47 +1,63 @@
 package examples;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 public class CalendarHandling {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         driver.get("https://www.path2usa.com/travel-companion/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        //driver.quit();
     }
 
     @Test
     public void gettingTheRightDateOnTheCalendar(){
 
-        String year;
-        String month;
-        String day;
+        String sYear = "2030";
+        String sMonth = "October";
+        String sDay = "28";
 
+        Util.scrowDownTo(driver.findElement(By.xpath("//div[@data-id='7a51525']")), driver);
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("form-field-travel_comp_date"))));
         driver.findElement(By.id("form-field-travel_comp_date")).click();
 
         List<WebElement> days = driver.findElements(By.xpath("//span[@class='flatpickr-day ']"));
-        WebElement nextMonth = driver.findElement(By.cssSelector(".flatpickr-next-month"));
-        WebElement nextYear = driver.findElement(By.cssSelector("arrowUp"));
+        Util.selectDay(sDay,days);
 
-        WebElement currentMonth = driver.findElement(By.cssSelector("cur-month"));
-        WebElement currentYear = driver.findElement(By.xpath("//input[@class='numInput cur-year']"));
+        driver.findElement(By.id("form-field-travel_comp_date")).click();
+        WebElement eCurrentMonth = driver.findElement(By.cssSelector(".flatpickr-current-month"));
+        WebElement eNextMonth = driver.findElement(By.cssSelector(".flatpickr-next-month"));
+        Util.selectMonth(eCurrentMonth,eNextMonth,sMonth);
+
+        driver.findElement(By.id("form-field-travel_comp_date")).click();
+        WebElement eNextYear = driver.findElement(By.cssSelector(".arrowUp"));
+        WebElement eCurrentYear = driver.findElement(By.xpath("//input[@class='numInput cur-year']"));
+        Util.selectYear(eCurrentYear, sYear, eNextYear);
 
     }
 
--- src/test/java/examples/Util.java
+++ src/test/java/examples/Util.java
@@ -1,6 +1,11 @@
 package examples;
 
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Util {
 
@@ -12,6 +17,59 @@ public class Util {
     public static void clickFor(WebElement element, int quantity) {
         for (int i = 1; i < quantity; i++) {
             element.click();
+        }
+    }
+
+    public static void scrowDownTo(WebElement element, WebDriver driver){
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    static void selectDay(String sDay, List<WebElement> oDays) {
+        boolean bDayFound = false;
+
+        for(WebElement day:oDays){
+            if (day.getText().equals(sDay)){
+                day.click();
+                bDayFound = true;
+                break;
+            }
+        }
+
+        if(!bDayFound){
+            Assert.fail("Day "+ sDay +" wasn't found");
+        }
+    }
+
+    public static void selectMonth(WebElement eCurrentMonth, WebElement eNextMonth,String sMonth) {
+        boolean bMonthFound = false;
+        for(int i=0; i<11; i++) {
+            if(!eCurrentMonth.getText().contains(sMonth)){
+                eNextMonth.click();
+            }else{
+                bMonthFound = true;
+                break;
+            }
+        }
+
+        if(!bMonthFound){
+            Assert.fail("Month "+ bMonthFound +" wasn't found");
+        }
+    }
+
+    public static void selectYear(WebElement eCurrentYear, String sYear, WebElement eNextYear) {
+        boolean bYearFound = false;
+
+        for (int i=0;i<20;i++){
+            if(!eCurrentYear.getText().equals(sYear)){
+                eNextYear.click();
+            }else{
+                bYearFound = true;
+                break;
+            }
+        }
+
+        if(!bYearFound){
+            Assert.fail("Year "+ bYearFound +" wasn't found");
         }
     }
 
