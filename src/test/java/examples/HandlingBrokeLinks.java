5daf9b8e0535161050b16c587b53d6dfa9cbd2e0
Gustavo Henrique <gustavo_henriquejr@live.com>
2023-12-13 22:40:42 +0000

    Updating my classes

---
+++ src/test/java/examples/HandlingBrokeLinks.java
@@ -0,0 +1,63 @@
+package examples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class HandlingBrokeLinks {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void checkingBrokeLinks() throws IOException {
+
+        List<WebElement> links = driver.findElements(By.xpath("//li[@class='gf-li']/a "));
+        String url;
+        int response;
+        HttpURLConnection connection;
+        SoftAssert a = new SoftAssert();
+
+        for(WebElement link:links){
+            url=link.getAttribute("href");
+            connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.connect();
+            response = connection.getResponseCode();
+            a.assertTrue(response<400,"Link "+link.getText()+" is returning "+response);
+            System.out.println(response);
+        }
+
+        a.assertAll();
+
+
+    }
+
+}
